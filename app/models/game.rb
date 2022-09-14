d89a2bd822b4ecef8b28e1f533b56801745b8e4d
Kirill Kuznetsov <shapowgorillalord@gmail.com>
2022-09-14 10:52:50 +0300

    refactor

--- app/models/game.rb
+++ app/models/game.rb
@@ -122,21 +122,42 @@ class Game < ActiveRecord::Base
     finish_game!(previous_level > -1 ? PRIZES[previous_level] : 0, false)
   end
 
+  # TODO: дорогой ученик!
+  # Код метода ниже можно сократиь в 3 раза с помощью возможностей Ruby и Rails,
+  # подумайте как и реализуйте. Помните о безопасности и входных данных!
+  #
+  # Вариант решения вы найдете в комментарии в конце файла, отвечающего за настройки
+  # хранения сессий вашего приложения. Вот такой вот вам ребус :)
+
   # Создает варианты подсказок для текущего игрового вопроса.
   # Возвращает true, если подсказка применилась успешно,
   # false если подсказка уже заюзана.
   #
   # help_type = :fifty_fifty | :audience_help | :friend_call
   def use_help(help_type)
-    help_types = %i[fifty_fifty audience_help friend_call]
-    help_type = help_type.to_sym
-    raise ArgumentError, 'wrong help_type' unless help_types.include?(help_type)
-
-    unless self["#{help_type}_used"]
-      self["#{help_type}_used"] = true
-      current_game_question.apply_help!(help_type)
-      save
+    case help_type
+    when :fifty_fifty
+      unless fifty_fifty_used
+        # ActiveRecord метод toggle! переключает булевое поле сразу в базе
+        toggle!(:fifty_fifty_used)
+        current_game_question.add_fifty_fifty
+        return true
+      end
+    when :audience_help
+      unless audience_help_used
+        toggle!(:audience_help_used)
+        current_game_question.add_audience_help
+        return true
+      end
+    when :friend_call
+      unless friend_call_used
+        toggle!(:friend_call_used)
+        current_game_question.add_friend_call
+        return true
+      end
     end
+
+    false
   end
 
   # Результат игры, одно из:
@@ -169,7 +190,7 @@ class Game < ActiveRecord::Base
 
   # Метод завершатель игры
   # Обновляет все нужные поля и начисляет юзеру выигрыш
-  def finish_game!(amount = 0, failed: true)
+  def finish_game!(amount = 0, failed = true)
     # оборачиваем в транзакцию - игра заканчивается
     # и баланс юзера пополняется только вместе
     transaction do
