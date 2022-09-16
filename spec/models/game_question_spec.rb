549bb18438321735feea9c923de89b0f93b4afb0
Kirill Kuznetsov <shapowgorillalord@gmail.com>
2022-09-16 12:22:36 +0300

    game question tests refactor

--- spec/models/game_question_spec.rb
+++ spec/models/game_question_spec.rb
@@ -59,16 +59,80 @@ RSpec.describe GameQuestion, type: :model do
   # }
   #
 
+  describe '#help_hash' do
+    it 'should return empty hash if help is not used' do
+      expect(game_question.help_hash).to eq({})
+    end
+
+    it 'should save help hash correctly' do
+      game_question.help_hash[:some_key1] = 'blabla1'
+      game_question.help_hash['some_key2'] = 'blabla2'
+
+      expect(game_question.save).to be_truthy
+
+      gq = GameQuestion.find(game_question.id)
+
+      expect(gq.help_hash).to eq({ some_key1: 'blabla1', 'some_key2' => 'blabla2' })
+    end
+  end
+
   context 'user helpers' do
-    it 'correct audience_help' do
+    it 'should not contain audience help in help hash before help is used' do
       expect(game_question.help_hash).not_to include(:audience_help)
+    end
 
-      game_question.add_audience_help
+    it 'should not contain friend call in help hash before help is used' do
+      expect(game_question.help_hash).not_to include(:friend_call)
+    end
 
-      expect(game_question.help_hash).to include(:audience_help)
+    it 'should not contain fifity fifty help in help hash before help is used' do
+      expect(game_question.help_hash).not_to include(:fifty_fifty)
+    end
 
-      ah = game_question.help_hash[:audience_help]
-      expect(ah.keys).to contain_exactly('a', 'b', 'c', 'd')
+    describe '#add_fifty_fifty' do
+      before { game_question.add_fifty_fifty }
+
+      it 'should include fifty fifty help in help hash' do
+        expect(game_question.help_hash).to include(:fifty_fifty)
+      end
+
+      it 'should contain correct answer' do
+        expect(game_question.help_hash[:fifty_fifty]).to include('b')
+      end
+
+      it 'should contain 2 variants' do
+        expect(game_question.help_hash[:fifty_fifty].size).to eq(2)
+      end
+    end
+
+    describe '#add_friend_call' do
+      before { game_question.add_friend_call }
+      let(:friend_guess) { game_question.help_hash[:friend_call] }
+
+      it 'should include friend call help in help hash' do
+        expect(game_question.help_hash).to include(:friend_call)
+      end
+
+      it 'should contain string' do
+        expect(friend_guess).to be_a String
+      end
+
+      it 'should contain guess of friend' do
+        expect(friend_guess[-1].downcase).to match(/[abcd]/)
+      end
+    end
+
+    describe '#add_audience_help' do
+      before { game_question.add_audience_help }
+
+      it 'should include audience help in help hash' do
+        expect(game_question.help_hash).to include(:audience_help)
+      end
+
+      it 'should contain all variants of answers as keys' do
+        ah = game_question.help_hash[:audience_help]
+        expect(ah.keys).to contain_exactly('a', 'b', 'c', 'd')
+      end
     end
   end
 end
